e7c26d4e482dc3d83fe93351e90e6a10813576ce
Samuel Javelle <samueljavelle@hotmail.com>
2023-05-16 21:22:11 +0200

    déplacements sur la carte

--- components/tile.js
+++ components/tile.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 function Tile(props) {
-  console.log(props.card)
+  //console.log(props.card)
   let playable = (<div style={{position: 'absolute', zIndex: '2', width: '100px', height: '100px'}}></div>)
   if(props.isPlayable) playable = (<div style={{position: 'absolute', zIndex: '2', width: '100px', height: '100px', backgroundColor: 'green', opacity: '.2'}}></div>)
   let rotationStyle = `rotate(${props.card.rotation * 90}deg)`
@@ -16,11 +16,11 @@ function Tile(props) {
       src={`/tuiles/${props.card.img}.png`}
       />
     {playable}
-      <div style={{width: '100px'}}>&nbsp;&nbsp;{props.card.data[0]}</div>
+      {/* <div style={{width: '100px'}}>&nbsp;&nbsp;{props.card.data[0]}</div>
       <div style={{width: '100px', justifyContent: 'space-between'}}>
         <p style={{textAlign: 'center'}}>{props.card.data[1]}</p><p style={{textAlign: 'center'}}>{props.id}</p><p style={{textAlign: 'center'}}>{props.card.data[3]}</p>
       </div>
-      <div style={{width: '100px', textAlign: 'end'}}>{props.card.data[2]}&nbsp;&nbsp;</div>
+      <div style={{width: '100px', textAlign: 'end'}}>{props.card.data[2]}&nbsp;&nbsp;</div> */}
     </div>
   );
 }
