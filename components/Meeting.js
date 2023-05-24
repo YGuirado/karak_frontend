07e235e10ef4dfa0adf33b5dc13e051eceb5582f
Yoann Guirado <yguirado@gmail.com>
2023-05-24 17:03:43 +0200

    final

--- components/Meeting.js
+++ components/Meeting.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 //import styles from '../styles/Meeting.module.css';
 import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDice, faDiceOne, faDiceTwo, faDiceThree, faDiceFour, faDiceFive, faDiceSix } from '@fortawesome/free-solid-svg-icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { useKey, updateInventory, looseLife } from '../reducers/inventory';
 import { updateMeet } from '../reducers/meeting';
